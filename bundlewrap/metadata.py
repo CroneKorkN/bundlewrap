044c6805a4052649657568e3e73981155eb1d42e
mwiegand <mwiegand@seibert-media.net>
2021-10-29 17:44:57 +0200

    dry deepcopy_metadata()

--- bundlewrap/metadata.py
+++ bundlewrap/metadata.py
@@ -4,7 +4,7 @@ from json import dumps, JSONEncoder
 
 from .exceptions import RepositoryError
 from .utils import Fault
-from .utils.dicts import ATOMIC_TYPES, map_dict_keys, merge_dict, value_at_key_path
+from .utils.dicts import _Atomic, ATOMIC_TYPES, map_dict_keys, merge_dict, value_at_key_path
 from .utils.text import force_text, mark_for_translation as _
 
 
@@ -32,31 +32,19 @@ def deepcopy_metadata(obj):
     if isinstance(obj, METADATA_TYPES):
         return obj
     elif isinstance(obj, dict):
-        if isinstance(obj, ATOMIC_TYPES[dict]):
-            new_obj = atomic({})
-        else:
-            new_obj = {}
-        for key, value in obj.items():
-            new_key = copy(key)
-            new_obj[new_key] = deepcopy_metadata(value)
-    elif isinstance(obj, (list, tuple)):
-        if isinstance(obj, (ATOMIC_TYPES[list], ATOMIC_TYPES[tuple])):
-            new_obj = atomic([])
-        else:
-            new_obj = []
-        for member in obj:
-            new_obj.append(deepcopy_metadata(member))
-        if isinstance(obj, tuple):
-            new_obj = tuple(new_obj)
-    elif isinstance(obj, set):
-        if isinstance(obj, ATOMIC_TYPES[set]):
-            new_obj = atomic(set())
-        else:
-            new_obj = set()
-        for member in obj:
-            new_obj.add(deepcopy_metadata(member))
+        new_obj = {
+            copy(key): deepcopy_metadata(value) for key, value in obj.items()
+        }
+    elif isinstance(obj, (list, tuple, set)):
+        new_obj = type(obj)(
+            deepcopy_metadata(value) for value in obj
+        )
     else:
         assert False  # there should be no other types
+
+    if isinstance(obj, _Atomic):
+        new_obj = atomic(new_obj)
+
     return new_obj
 
 
